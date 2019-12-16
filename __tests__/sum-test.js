63ca04b227d1944b48b4a285dcc68069d3c40e5e
Rong Liu <rongliu@Rongs-Mac-mini.local>
2019-12-16 12:45:16 +0800

    test

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -3,6 +3,6 @@ jest.dontMock('../src/sum');
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
     var sum = require('../src/sum');
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(4);
   });
 });
